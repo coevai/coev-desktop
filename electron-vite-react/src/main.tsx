cd1fdd5145516f0008a6b683457f7633588c0b47
Josh pf <jporrittfraser@gmail.com>
2024-08-12 12:57:38 +1000

    fix stuff

--- electron-vite-react/src/main.tsx
+++ electron-vite-react/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
+import './tailwind.css'
 
 import './demos/ipc'
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
